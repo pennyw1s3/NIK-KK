90bbc92fce713d8fc16af87e940d6db34a99ef94
pennyw1s3 <50727921+pennyw1s3@users.noreply.github.com>
2019-06-26 08:48:45 +0700

    Add files via upload

---
+++ var.php
@@ -0,0 +1,45 @@
+<?php
+$white = "\e[97m";
+$black = "\e[30m\e[1m";
+$yellow = "\e[93m";
+$orange = "\e[38;5;208m";
+$blue   = "\e[34m";
+$lblue  = "\e[36m";
+$cln    = "\e[0m";
+$green  = "\e[92m";
+$fgreen = "\e[32m";
+$red    = "\e[91m";
+$magenta = "\e[35m";
+$bluebg = "\e[44m";
+$lbluebg = "\e[106m";
+$greenbg = "\e[42m";
+$lgreenbg = "\e[102m";
+$yellowbg = "\e[43m";
+$lyellowbg = "\e[103m";
+$redbg = "\e[101m";
+$grey = "\e[37m";
+$cyan = "\e[36m";
+$bold   = "\e[1m";
+function kkktp_banner(){
+  echo "\e[91;1m
+ 
+ NNNNNNNN        NNNNNNNNIIIIIIIIIIKKKKKKKKK    KKKKKKK          &&&&&&&&&&         KKKKKKKKK    KKKKKKKKKKKKKKKK    KKKKKKK
+ N:::::::N       N::::::NI::::::::IK:::::::K    K:::::K         &::::::::::&        K:::::::K    K:::::KK:::::::K    K:::::K
+ N::::::::N      N::::::NI::::::::IK:::::::K    K:::::K        &::::&&&:::::&       K:::::::K    K:::::KK:::::::K    K:::::K
+ N:::::::::N     N::::::NII::::::IIK:::::::K   K::::::K       &::::&   &::::&       K:::::::K   K::::::KK:::::::K   K::::::K
+ N::::::::::N    N::::::N  I::::I  KK::::::K  K:::::KKK       &::::&   &::::&       KK::::::K  K:::::KKKKK::::::K  K:::::KKK
+ N:::::::::::N   N::::::N  I::::I    K:::::K K:::::K           &::::&&&::::&          K:::::K K:::::K     K:::::K K:::::K   
+ N:::::::N::::N  N::::::N  I::::I    K::::::K:::::K            &::::::::::&           K::::::K:::::K      K::::::K:::::K    
+ N::::::N N::::N N::::::N  I::::I    K:::::::::::K              &:::::::&&            K:::::::::::K       K:::::::::::K     
+ N::::::N  N::::N:::::::N  I::::I    K:::::::::::K            &::::::::&   &&&&       K:::::::::::K       K:::::::::::K     
+ N::::::N   N:::::::::::N  I::::I    K::::::K:::::K          &:::::&&::&  &:::&       K::::::K:::::K      K::::::K:::::K    
+ N::::::N    N::::::::::N  I::::I    K:::::K K:::::K        &:::::&  &::&&:::&&       K:::::K K:::::K     K:::::K K:::::K   
+ N::::::N     N:::::::::N  I::::I  KK::::::K  K:::::KKK     &:::::&   &:::::&       KK::::::K  K:::::KKKKK::::::K  K:::::KKK
+ N::::::N      N::::::::NII::::::IIK:::::::K   K::::::K     &:::::&    &::::&       K:::::::K   K::::::KK:::::::K   K::::::K
+ N::::::N       N:::::::NI::::::::IK:::::::K    K:::::K     &::::::&&&&::::::&&     K:::::::K    K:::::KK:::::::K    K:::::K
+ N::::::N        N::::::NI::::::::IK:::::::K    K:::::K      &&::::::::&&&::::&     K:::::::K    K:::::KK:::::::K    K:::::K
+ NNNNNNNN         NNNNNNNIIIIIIIIIIKKKKKKKKK    KKKKKKK        &&&&&&&&   &&&&&     KKKKKKKKK    KKKKKKKKKKKKKKKK    KKKKKKK
+ 
+  \n";
+}
+?>
